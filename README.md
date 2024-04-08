01c7f06b54c0993908ef9e8aa582ab50b8d2a536
AbuBokorprog <abubokor1066@gmail.com>
2024-04-08 08:33:18 +0600

    express install

---
+++ README.md
@@ -0,0 +1,10 @@
+# Express.JS
+
+Fast, unopinionated, minimalist web framework for Node.js
+
+## Installation:
+
+```
+npm install express OR
+yarn add express
+```
---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+
+app.use("/", (req, res) => {
+  res.send("Welcome");
+});
+
+app.listen(port, () => {
+  console.log("listening on port 5000");
+});
