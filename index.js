54eeefc617fcdcb75659cb11e7df01e68aa230a2
AbuBokorprog <abubokor1066@gmail.com>
2024-04-08 10:15:41 +0600

    express function

--- index.js
+++ index.js
@@ -2,8 +2,14 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-app.use("/", (req, res) => {
+app.use(express.json());
+
+app.get("/", (req, res) => {
   res.send("Welcome");
+});
+app.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("Home page post request");
 });
 
 app.listen(port, () => {
